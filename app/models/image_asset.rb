20a24a3bd5e1504d9b08edfbcba4bacb6fe8b7e8
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-02-11 22:06:55 -0800

    got my local site working at least

--- app/models/image_asset.rb
+++ app/models/image_asset.rb
@@ -2,6 +2,11 @@ class ImageAsset < ActiveRecord::Base
 	belongs_to :attachable
 	has_attached_file :attachment, 
   	:styles => {:large => "x600>", :medium => "x300>", :thumb => "x100>" }, 
-  	:default_url => "/images/missing-image-:style.jpg"
+  	:default_url => "/images/missing-image-:style.jpg"										,
+    :storage        => :s3                                                ,
+	  :s3_credentials => {:bucket            => ENV['AWS_BUCKET'           ],
+	                      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'    ],
+	                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+	  :s3_protocol    => "https"                  
   attr_accessible :attachment, :user_id, :attachable_id, :attachment_attributes
 end

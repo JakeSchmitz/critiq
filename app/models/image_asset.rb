e55bb71f9edcb3e1b3590bcb65ba2be3b3ef185b
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-02-14 19:36:47 -0800

    more configshit

--- app/models/image_asset.rb
+++ app/models/image_asset.rb
@@ -4,10 +4,10 @@ class ImageAsset < ActiveRecord::Base
   	:styles => {:large => "x600>", :medium => "x300>", :thumb => "x100>" }, 
   	:default_url => "/images/missing-image-:style.jpg"										,
     :storage        => :s3                                                ,
-	  :s3_credentials => {:bucket            => ENV['AWS_BUCKET']				,
+	  :s3_credentials => {:s3_endpoint 		=> 's3-us-west-2.amazonaws.com' ,
+	  										:bucket            => ENV['AWS_BUCKET']				,
 	                      :access_key_id     => ENV['AWS_ACCESS_KEY_ID'    ],
 	                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
-	  :s3_endpoint 		=> 's3-us-west-2.amazonaws.com' ,
 	  :s3_protocol    => "https"                  
   attr_accessible :attachment, :user_id, :attachable_id, :attachment_attributes
 end

27ac7d15b11bb02fe3aff5a5a6f1f82804d080d4
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-04-04 18:49:33 -0700

    font issues

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module Critiq0
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("system", "image_assets", "images", "assets/SlidePushMenus")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

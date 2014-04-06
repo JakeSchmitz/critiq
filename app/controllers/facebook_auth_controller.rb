c1379236f6c45089b93f76a43c968f615d7b37da
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-04-06 16:29:25 -0700

    still fb fails

--- app/controllers/facebook_auth_controller.rb
+++ app/controllers/facebook_auth_controller.rb
@@ -1,17 +1,18 @@
 class FacebookAuthController < ApplicationController
 	include SessionsHelper
 	def new
-		@fb_params = Koala::Facebook::API.new(request.env['omniauth.auth']['credentials'].token)
+		fbcode = request.env['omniauth.auth']['credentials'].token
+		@fb_params = Koala::Facebook::API.new(fbcode)
 		profile = @fb_params.get_object("me")
-		if !User.where email: profile["email"].empty?
-			@user = User.create(name: profile["name"], email: profile["email"], password: rand_pwd)
+		if User.where(email: profile["email"]).empty?
+			@user = User.create(name: profile["name"], email: profile["email"], password: fbcode)
 			@user.save
 		else
 			@user = User.find_by email: profile["email"]
 			current_user=@user
 		end
 		if @user
-			fb_sign_in(@user, request.env['omniauth.auth']['credentials'].token)
+			fb_sign_in(@user, fbcode)
 			redirect_to @user
 		else
 			redirect_to signin_url, notice: "Facebook authentication failed! Please sign in with email and password"

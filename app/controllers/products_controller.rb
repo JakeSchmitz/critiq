974275c2a7c18b0d415dcac8aaffcfead7b133b2
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-04-07 15:30:40 -0700

    image blur fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  require 'rmagick'
-
   # GET /products
   # GET /products.json
   def index

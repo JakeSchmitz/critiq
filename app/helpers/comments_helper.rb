c92845509bec624e594cbdc56197b3223586fc49
hzla00 <neohzla@gmail.com>
2014-04-13 23:29:48 -0700

    fixed collapsed bug

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,18 +4,17 @@ module CommentsHelper
   		comments.map do |comment, sub_comments|
 	    	content_tag(:div,  (render partial: '/comments/comment',
 	    	 locals: {comment: comment, product: product, commentable: product, reply: [product, comment] }) + 
-	    	content_tag(:div, nested_comments(sub_comments, product), :class => "nested_comments") + 
-	    	content_tag(:div, (render partial: '/comments/collapsed_comment', locals: {comment: comment}), :class => "collapsed" ), 
+	    	content_tag(:div, nested_comments(sub_comments, product), :class => "nested_comments"), 
 	    	:class => "comment-nest")
   		end.join.html_safe
 	end
+
 
 	def nested_bounty_comments comments, product, bounty
 		comments.map do |comment, sub_comments|
 	    	content_tag(:div,  (render partial: '/comments/comment',
 	    	 locals: {comment: comment, product: product, commentable: bounty, reply: [product, bounty, comment] }) + 
-	    	content_tag(:div, nested_comments(sub_comments, product), :class => "nested_comments") + 
-	    	content_tag(:div, (render partial: '/comments/collapsed_comment', locals: {comment: comment}), :class => "collapsed" ), 
+	    	content_tag(:div, nested_comments(sub_comments, product), :class => "nested_comments"), 
 	    	:class => "comment-nest")
   		end.join.html_safe
 	end

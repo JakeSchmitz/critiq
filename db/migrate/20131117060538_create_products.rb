4c4a0fb11d8342c1bb221d491cd87656bea58990
Jacob Schmitz <jake.schmitz101@gmail.com>
2014-04-25 20:43:11 -0700

    responsive product pages

--- db/migrate/20131117060538_create_products.rb
+++ db/migrate/20131117060538_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.string :image
-      t.text :description
+      t.text :description, limit: 1000
 
       t.timestamps
     end
